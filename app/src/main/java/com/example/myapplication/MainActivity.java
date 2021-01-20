a0eabed16313aa5f121c6ad14ac3900a025abd60
Ahmed Fouad <ahmedfouad01099@gmail.com>
2021-01-20 03:54:12 +0200

    adding another text

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void clickFunction(View view) {
+
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        EditText passwdEditText = (EditText) findViewById(R.id.passwdEditText2);
+        Log.i("key", "Button Clicked!!");
+        Log.i("values", nameEditText.getText().toString());
+        Log.i("values", passwdEditText.getText().toString());
+        Toast.makeText(this, nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, passwdEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+
+    }
+
+
 }
